0c4e925b1dc8d0129074a1e08082b8d5decfb300
Doug Turnbull <softwaredoug@gmail.com>
2024-03-22 09:41:13 -0400

    Fix snowball tokenizer

--- msmarco/tokenizers.py
+++ msmarco/tokenizers.py
@@ -4,7 +4,7 @@ import string
 stemmer = Stemmer.Stemmer('english', maxCacheSize=0)
 
 fold_to_ascii = dict([(ord(x), ord(y)) for x, y in zip(u"‘’´“”–-", u"'''\"\"--")])
-punct_trans = str.maketrans('', '', string.punctuation)
+punct_trans = str.maketrans({key: ' ' for key in string.punctuation})
 all_trans = {**fold_to_ascii, **punct_trans}
 
 
